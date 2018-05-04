dfa0d9e449eca6efa35d265e1f6e892a679c6fef
jystinz <jystinz.kn@gmail.com>
2018-05-05 03:18:41 +0700

    Ep 15

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,8 +3,26 @@
 @section('content')
 
     <div class="col-sm-8 blog-main">
+
         <h1>{{ $post->title }}</h1>
+
         {{ $post->body }}
+
+        <hr>
+
+        <div class="comments">
+            <ui class="list-group">
+                @foreach($post->comments as $comment)
+                    <li class="list-group-item">
+                        <strong>
+                            {{ $comment->created_at->diffForHumans() }}: &nbsp;
+                        </strong>
+                        {{ $comment->body }}
+                    </li>
+                @endforeach
+            </ui>
+        </div>
+
     </div>
 
 @endsection
