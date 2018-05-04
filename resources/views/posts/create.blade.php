206edf87a3ef6fc9282737cb84163986c67a9844
jystinz <jystinz.kn@gmail.com>
2018-05-05 00:56:39 +0700

    Ep 11

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="col-sm-8 blog-main">
+
+        <h1>Publish a Post</h1>
+
+        <hr>
+
+        <form action="/posts" method="post">
+
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <label for="title">Title:</label>
+                <input type="text" name="title" id="title" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="body">Body:</label>
+                <textarea name="body" id="body" class="form-control"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Publish</button>
+
+        </form>
+
+    </div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+\Illuminate\Support\Facades\Route::get('/', 'PostsController@index');
+\Illuminate\Support\Facades\Route::get('/posts/create', 'PostsController@create');
+\Illuminate\Support\Facades\Route::post('/posts', 'PostsController@store');
+//\Illuminate\Support\Facades\Route::get('/posts/{post}', 'PostsController@show');
