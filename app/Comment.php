50d784a966c95ac879a76600985664799d394653
jystinz <jystinz.kn@gmail.com>
2018-05-05 15:46:47 +0700

    Ep 18

--- app/Comment.php
+++ app/Comment.php
@@ -6,6 +6,13 @@ class Comment extends Model
 {
     public function post()
     {
+
         return $this->belongsTo(Post::class);
+    }
+
+    public function user() // $comment->user->name
+    {
+
+        return $this->belongsTo(User::class);
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -10,6 +10,12 @@ class Post extends Model
         return $this->hasMany(Comment::class);
     }
 
+    public function user() // $comment->post->user
+    {
+
+        return $this->belongsTo(User::class);
+    }
+
     public function addComment($body)
     {
         $this->comments()->create(compact('body'));
